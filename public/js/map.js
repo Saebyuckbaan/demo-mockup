fd0612ebcae3993ca83bb8b08a1a4cec2db82e9e
Elaine Hu <eyhu@ucsd.edu>
2016-04-26 19:03:50 -0700

    Updated index.html; map.js now produces google map with overlay; css for the shenanigans.

--- public/js/map.js
+++ public/js/map.js
@@ -1,71 +1,98 @@
 (function(d3) {
   "use strict";
-
   
+  // Google Map
+  var map = new google.maps.Map(d3.select("#map").node(), {
+    zoom: 9,
+    minZoom: 6,
+    center: new google.maps.LatLng(33.0157, -116.8611),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
 
-  var width = 1200;
-  var height = 800;
-  var svg = d3.select("body")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height);
-
+  // Colors for the values; currently gives arbitrary colors
   var color = d3.scale.quantize()
-                    .range(["rgb(237,248,233)", "rgb(186,228,179)",
-                     "rgb(116,196,118)", "rgb(49,163,84)","rgb(0,109,44)"]);
+    .range(["#ffffd9","#edf8b1","#c7e9b4",
+            "#7fcdbb","#41b6c4","#1d91c0",
+            "#225ea8","#253494","#081d58"]);
 
+  // JSON-related
   d3.json("https://raw.githubusercontent.com/leonlxli/Cogs121_P2_Uber/master/public/map/sdcounty.json",
-    function(json) {
-      console.log(json);
-
-      var center = d3.geo.centroid(json);
-      var scale = 25000;
-      var offset = [width/2, height/2];
-    
-      var projection = d3.geo.mercator()
-        .scale(scale)
-        .center(center)
-        .translate(offset);
-      var path = d3.geo.path().projection(projection);
-      var bounds = path.bounds(json);
-
-      for (var i = 0; i < json.features.length; i++) {
-        // Gets individual city objects
-        // to get NAME: var SDcityName = json.features[i].properties.NAME;
-        var SDcity = json.features[i];
-        json.features[i].properties.VALUE = i;
-        console.log(SDcity);
+    function(error, json) {
+      // if an error occurs, THROW AN ERROR
+      if (error) {
+        console.error(error);
+        throw error;
       }
 
-      svg.selectAll("path")
-        .data(json.features)
-        .enter()
-        .append("path")
-        .attr("d", path)
-        .style("fill", function(d) {
-          var val = d.properties.VALUE;
-          return color(val/100);
-        })
-        .style("stroke", "blue")
-        .append("title")
-        .text(function(d) {return d.properties.NAME;});
+      // prints all objects in the json file
+      // console.log(json);
 
-        svg.selectAll("path")
-        .on("mouseover", function(d) {
-          console.log(d);
-          d3.select(this)
-            .style("stroke", "black")
-            .style("fill", "red");
-        })
-        .on("mouseout", function(d) {
-          console.log(d);
-          d3.select(this)
-            .style("stroke", "blue")
-            .style("fill", function(d) {
-              var val = d.properties.VALUE;
-              return color(val/100);
-            });
-        });
+      // Creates the overlay
+      var overlay = new google.maps.OverlayView();
+
+      overlay.onAdd = function() {
+        var layer = d3.select(this.getPanes().overlayLayer).append("div")
+          .attr("class", "cities");
+
+        var svg = layer.append("svg");
+        var cities = svg.append("g").attr("class", "cityDiv");
+
+        // Assigning random value to a city
+        for (var i = 0; i < json.features.length; i++) {
+          // Gets individual city objects
+          // to get NAME: var SDcityName = json.features[i].properties.NAME;
+          var SDcity = json.features[i];
+          json.features[i].properties.VALUE = i;
+          console.log(SDcity);
+        }
+
+          overlay.draw = function() {
+            var projection = this.getProjection();
+
+            var googleMapProjection = function(coord) {
+              var googleCoord =  new google.maps.LatLng(coord[1], coord[0]);
+              var pixCoord = projection.fromLatLngToDivPixel(googleCoord);
+              return [pixCoord.x + 4000, pixCoord.y + 4000];
+            }
+
+            var path = d3.geo.path().projection(googleMapProjection);
+            cities.selectAll("path")
+              .data(json.features)
+              .attr("d", path)
+              .enter()
+              .append("svg:path")
+              .attr("d", path)
+              .style("fill", function(d) {
+                var val = d.properties.VALUE;
+                return color(val/41); // return "#000000"; for no data
+              })
+              .style("stroke", "blue")
+              .append("title")
+              .text(function(d) {return d.properties.NAME;});
+
+            // Changes color over hover; CURRENTLY NOT WORKING
+            cities.selectAll("path")
+              .on("mouseover", function(d) {
+                console.log("WHY");
+                d3.select(this)
+                  .style("stroke", "black")
+                  .style("fill", "red");
+              })
+              .on("mouseout", function(d) {
+                console.log("Sigh.");
+                d3.select(this)
+                  .style("stroke", "blue")
+                  .style("fill", function(d) {
+                    var val = d.properties.VALUE;
+                    return color(val/41);
+                  });
+              });  // End Hover-related shenanigans
+            
+          }; // END DRAW
+
+      }; // END ON ADD
+
+      overlay.setMap(map);
     }
   );
 
