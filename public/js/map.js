161a11b1050faec2f2a6dd08459cd9b8fa9a58de
Koshinryo <jpg007@ucsd.edu>
2016-04-27 21:21:50 -0700

    lotsa nothing

--- public/js/map.js
+++ public/js/map.js
@@ -210,32 +210,6 @@
                     */
                 });  // End Hover-related shenanigans
 
-                var legendRectSize = 18;                                  // NEW
-                var legendSpacing = 4;
-
-                var legend = svg.selectAll('.legend')
-                  .data(color.domain())
-                  .enter()
-                  .append('g')
-                  .attr('class', 'legend')
-                  .attr('transform', function(d, i) {
-                    var height = legendRectSize + legendSpacing;
-                    var offset = height * color.domain().length/2
-                    var horz = -2 * legendRectSize;
-                    var vert = i * height - offset;
-                    return 'translate(' + horz + ',' + vert + ')';
-                  })
-
-                  legend.append('rect')
-                  .attr('width', legendRectSize)
-                  .attr('height', legendRectSize)
-                  .style('fill', color)
-                  .style('stroke', color);
-
-                  legend.append('text')
-                  .attr('x', legendRectSize + legendSpacing)
-                  .attr('y', legendRectSize - legendSpacing)
-                  .text(function(d) { return d; });
             });
 
           }; // END DRAW
